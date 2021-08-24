fe87067328078ededf83ccaf3f657a0a4d4b1186
Anastasios Koumarelis <akoumarelis@gmail.com>
2021-08-24 14:24:36 +0300

    Update Helmet.c

--- Helmet.c
+++ Helmet.c
@@ -1,3 +1,7 @@
+//Developer: Anastaios Koumarelis (akoumarelis@gmail.com)
+//University: Hellenic Mediteranian University, Department of Electrical and Computer Engineering, Msc in Informatics Engineering 
+//Course: Internet of Things: Technologies and Applications 2020-2021
+
 //HEAD
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
